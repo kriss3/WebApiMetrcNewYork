30e9dc943156cc468dbc9d0c10e3120bcfe3f15d
Kris <kris_s2@outlook.com>
2025-11-02 11:12:51 -0800

    kata: working on DI in Program file.

--- WebApiMetrcNewYork.App/Program.cs
+++ WebApiMetrcNewYork.App/Program.cs
@@ -1,4 +1,8 @@
 
+using WebApiMetrcNewYork.App.Client;
+using WebApiMetrcNewYork.App.Models;
+using WebApiMetrcNewYork.App.Services;
+
 namespace WebApiMetrcNewYork.App;
 
 public class Program
@@ -6,7 +10,21 @@ public class Program
     public static void Main()
     {
         var builder = WebApplication.CreateBuilder();
-        builder.Services.AddControllers();
+
+		builder.Services.Configure<MetrcOptions>(builder.Configuration.GetSection("Metrc"));
+		builder.Services.AddTransient<MetrcAuthHandler>();
+
+
+		// Named client; BaseAddress is optional since I have absolute URLs.
+		builder.Services.AddHttpClient("MetrcNY", (sp, http) =>
+		{
+			http.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+		}).AddHttpMessageHandler<MetrcAuthHandler>();
+
+		builder.Services.AddScoped<IMetrcHttp, MetrcHttp>();
+		builder.Services.AddScoped<IMetrcDeliveriesService, MetrcDeliveriesService>();
+
+		builder.Services.AddControllers();
         builder.Services.AddOpenApi();
 
         var app = builder.Build();
@@ -20,7 +38,6 @@ public class Program
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
-
 
         app.MapControllers();
 
