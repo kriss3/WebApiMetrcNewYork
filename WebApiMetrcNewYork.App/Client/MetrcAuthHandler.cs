f795602a893611d18ed5456ded339a2c9fa580b4
Kris <kris_s2@outlook.com>
2025-11-02 10:44:15 -0800

    kata: adding confguration of basic auth when calling metrc api.

--- WebApiMetrcNewYork.App/Client/MetrcAuthHandler.cs
+++ WebApiMetrcNewYork.App/Client/MetrcAuthHandler.cs
@@ -1,5 +1,20 @@
-﻿namespace WebApiMetrcNewYork.App.Client;
+﻿using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Text;
+using WebApiMetrcNewYork.App.Models;
 
-public class MetrcAuthHandler
+namespace WebApiMetrcNewYork.App.Client;
+
+public sealed class MetrcAuthHandler(IOptions<MetrcOptions> opts) : DelegatingHandler
 {
+	private readonly MetrcOptions _opts = opts.Value;
+
+	protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken ct)
+	{
+		var token = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{_opts.VendorApiKey}:{_opts.UserApiKey}"));
+		request.Headers.Authorization = new AuthenticationHeaderValue("Basic", token);
+		request.Headers.Accept.ParseAdd("application/json");
+		
+		return base.SendAsync(request, ct);
+	}
 }
