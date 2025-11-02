17d46b3059a6f994a3517fb43f51934b83268dc6
Kris <kris_s2@outlook.com>
2025-11-02 10:50:30 -0800

    kata: adding basic metrc http endpoint support.

--- WebApiMetrcNewYork.App/Client/IMetrcHttp.cs
+++ WebApiMetrcNewYork.App/Client/IMetrcHttp.cs
@@ -2,4 +2,6 @@
 
 public interface IMetrcHttp
 {
+	Task<(int Status, string Body)> GetAsync(string absoluteUrl, CancellationToken ct = default);
+	Task<(int Status, string Body)> PostAsync(string absoluteUrl, string jsonBody, CancellationToken ct = default);
 }
