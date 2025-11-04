40f65356dc7beead768d3ce9f07182b5085c2221
Kris <kris_s2@outlook.com>
2025-11-03 23:29:57 -0800

    kata: adding response envelope.

--- WebApiMetrcNewYork.App/Client/IMetrcHttp.cs
+++ WebApiMetrcNewYork.App/Client/IMetrcHttp.cs
@@ -1,7 +1,18 @@
-﻿namespace WebApiMetrcNewYork.App.Client;
+﻿using WebApiMetrcNewYork.App.Models;
 
+namespace WebApiMetrcNewYork.App.Client;
+
+//public interface IMetrcHttp
+//{
+//	Task<(int Status, string Body)> GetAsync(string absoluteUrl, CancellationToken ct = default);
+//	Task<(int Status, string Body)> PostAsync(string absoluteUrl, string jsonBody, CancellationToken ct = default);
+//}
+
+/// <summary>
+/// This is the alternative approach with ApiEnvelop. Mimics Result<TSuccess, TFailure>
+/// </summary>
 public interface IMetrcHttp
 {
-	Task<(int Status, string Body)> GetAsync(string absoluteUrl, CancellationToken ct = default);
-	Task<(int Status, string Body)> PostAsync(string absoluteUrl, string jsonBody, CancellationToken ct = default);
+	Task<ApiEnvelope> GetAsync(string absoluteUrl, CancellationToken ct = default);
+	Task<ApiEnvelope> PostAsync(string absoluteUrl, string jsonBody, CancellationToken ct = default);
 }
