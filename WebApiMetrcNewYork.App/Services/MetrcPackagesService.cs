6c57a4b96542040c9e694059cb9d20112db85810
Kris <kris_s2@outlook.com>
2025-11-11 14:44:39 -0800

    kata: adding MetrcPackages ctor.

--- WebApiMetrcNewYork.App/Services/MetrcPackagesService.cs
+++ WebApiMetrcNewYork.App/Services/MetrcPackagesService.cs
@@ -1,11 +1,12 @@
-﻿using WebApiMetrcNewYork.App.Client;
+﻿using Microsoft.Extensions.Options;
+using WebApiMetrcNewYork.App.Client;
 using WebApiMetrcNewYork.App.Models;
 
 namespace WebApiMetrcNewYork.App.Services;
 
-public sealed class MetrcPackagesService : IMetrcPackagesService
+public sealed class MetrcPackagesService(IMetrcHttp http, IOptions<MetrcOptions> opts) 
+	: IMetrcPackagesService
 {
-	private readonly IMetrcHttp _http;
-	private readonly MetrcOptions _opts;
-
+	private readonly IMetrcHttp _http = http;
+	private readonly MetrcOptions _opts = opts.Value;
 }
