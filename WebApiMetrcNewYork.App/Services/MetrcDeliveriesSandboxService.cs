00f4412c4a6fcfe3cc47cd02a1856f48a6d8769e
Kris <kris_s2@outlook.com>
2025-11-11 14:55:00 -0800

    kata: initial service setup

--- WebApiMetrcNewYork.App/Services/MetrcDeliveriesSandboxService.cs
+++ WebApiMetrcNewYork.App/Services/MetrcDeliveriesSandboxService.cs
@@ -1,5 +1,17 @@
-﻿namespace WebApiMetrcNewYork.App.Services;
+﻿using Microsoft.Extensions.Options;
+using WebApiMetrcNewYork.App.Client;
+using WebApiMetrcNewYork.App.Models;
+
+namespace WebApiMetrcNewYork.App.Services;
 
 public sealed class MetrcDeliveriesSandboxService : IMetrcDeliveriesSandboxService
-{ 
+{
+	private readonly IMetrcHttp _http;
+	private readonly MetrcOptions _opts;
+
+	public MetrcDeliveriesSandboxService(IMetrcHttp http, IOptions<MetrcOptions> opts)
+	{
+		_http = http;
+		_opts = opts.Value;
+	}
 }
