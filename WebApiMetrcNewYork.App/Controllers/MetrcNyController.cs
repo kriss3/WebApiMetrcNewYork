f70ee9149682cabaffcfb716a38e3a5a236467ac
Kris <kris_s2@outlook.com>
2025-11-11 16:51:55 -0800

    kata: working on controller.

--- WebApiMetrcNewYork.App/Controllers/MetrcNyController.cs
+++ WebApiMetrcNewYork.App/Controllers/MetrcNyController.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApiMetrcNewYork.App.Services;
 
 namespace WebApiMetrcNewYork.App.Controllers;
 [Route("api/[controller]")]
 [ApiController]
-public class MetrcNyController : ControllerBase
+public sealed class MetrcNyController(
+    IMetrcPackagesService packages,
+    IMetrcDeliveriesSandboxService deliveriesSandbox) : ControllerBase
 {
+	private readonly IMetrcPackagesService _packages = packages;
+	private readonly IMetrcDeliveriesSandboxService _deliveriesSandbox = deliveriesSandbox;
+
+
+
+
+
+
 }
