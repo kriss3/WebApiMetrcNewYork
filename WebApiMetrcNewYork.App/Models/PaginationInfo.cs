7b85fdaf896c7e3e7511b89d1c858f8f7629a71c
Kris <kris_s2@outlook.com>
2025-11-12 21:43:27 -0800

    kata: attempt to encapsulate Pagination info returned from Metrc,

---
+++ WebApiMetrcNewYork.App/Models/PaginationInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiMetrcNewYork.App.Models;
+
+public sealed record PaginationInfo(
+	int? Total,
+	int? TotalRecords,
+	int? PageSize,
+	int? RecordsOnPage,
+	int? Page,
+	int? CurrentPage,
+	int? TotalPages
+);
