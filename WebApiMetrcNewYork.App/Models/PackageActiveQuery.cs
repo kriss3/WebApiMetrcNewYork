d93de871893d0f2fa5bb49adbaf1018d6dc1fe37
Kris <kris_s2@outlook.com>
2025-11-12 21:46:01 -0800

    kata: attempt to encapsulate query paramters.

---
+++ WebApiMetrcNewYork.App/Models/PackageActiveQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiMetrcNewYork.App.Models;
+
+public sealed class PackagesActiveQuery
+{
+	// Use DateOnly to naturally express "date without time"
+	public DateOnly? LastModifiedStartDate { get; init; }
+	public DateOnly? LastModifiedEndDate { get; init; }
+
+	public int? PageNumber { get; init; }
+	public int? PageSize { get; init; }
+}
