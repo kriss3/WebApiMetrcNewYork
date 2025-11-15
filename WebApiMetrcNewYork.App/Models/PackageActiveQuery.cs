faa64a44186ee2dd2f01b5cd403e0c4ac5c7c45e
Kris <kris_s2@outlook.com>
2025-11-15 08:58:40 -0800

    kata: enhancing common query param type to include Licens number and adding annotations to follow api requirements.

--- WebApiMetrcNewYork.App/Models/PackageActiveQuery.cs
+++ WebApiMetrcNewYork.App/Models/PackageActiveQuery.cs
@@ -1,25 +1,16 @@
-﻿namespace WebApiMetrcNewYork.App.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiMetrcNewYork.App.Models;
 
 public sealed class CommonQueryParams
 {
-
+	[Required]
+	public string LicenseNumber { get; init; } = default!;
+	
 	// Use DateOnly to naturally express "date without time"
 	public DateOnly? LastModifiedStart { get; init; }
 	public DateOnly? LastModifiedEnd { get; init; }
-
 	public int? PageNumber { get; init; }
+	[Range(1, 20)]
 	public int? PageSize { get; init; }
-
-
 }
-
-/*
- * [Required]
-    public string LicenseNumber { get; init; } = default!;
-    public DateOnly? LastModifiedStart { get; init; }
-    public DateOnly? LastModifiedEnd { get; init; }
-    public int? PageNumber { get; init; }
-    [Range(1, 20)]
-    public int? PageSize { get; init; }
- * 
- */
