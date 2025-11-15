ee153538db7a4da165b4b172648dc500cc9167c7
Kris <kris_s2@outlook.com>
2025-11-15 09:02:09 -0800

    kata: decided common query param only include data and page information. LicenseNumber already belongs to MetrcOption type.

--- WebApiMetrcNewYork.App/Models/CommonQueryParams.cs
+++ WebApiMetrcNewYork.App/Models/CommonQueryParams.cs
@@ -3,10 +3,7 @@
 namespace WebApiMetrcNewYork.App.Models;
 
 public sealed class CommonQueryParams
-{
-	[Required]
-	public string LicenseNumber { get; init; } = default!;
-	
+{	
 	// Use DateOnly to naturally express "date without time"
 	public DateOnly? LastModifiedStart { get; init; }
 	public DateOnly? LastModifiedEnd { get; init; }
