2368641dd8e3b0f80d2764f9e51f5a16794e230b
Kris <kris_s2@outlook.com>
2025-11-24 22:30:37 -0800

    kata: adding MetrcWebhooksService

---
+++ WebApiMetrcNewYork.App/Services/MetrcWebhooksService.cs
@@ -0,0 +1,16 @@
+﻿using WebApiMetrcNewYork.App.Models;
+
+namespace WebApiMetrcNewYork.App.Services;
+
+public sealed class MetrcWebhooksService : IMetrcWebhooksService
+{
+    public Task<ApiEnvelope> GetSubscriptionsAsync(CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ApiEnvelope> SubscribeToPackageWebhooksAsync(CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
