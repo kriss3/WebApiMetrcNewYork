7613d6e52eb6b571bb8f4e93a1fedc5d8e5692f9
Kris <kris_s2@outlook.com>
2025-11-24 22:39:07 -0800

    kata: adding GetSubscription implementation.

--- WebApiMetrcNewYork.App/Services/MetrcWebHooksService.cs
+++ WebApiMetrcNewYork.App/Services/MetrcWebHooksService.cs
@@ -10,18 +10,12 @@ public sealed class MetrcWebHooksService(
 	private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
 	private readonly MetrcOptions _opts = opts.Value;
 
-	public Task<ApiEnvelope> GetSubscriptionsAsync(CancellationToken ct)
+	public async Task<ApiEnvelope> GetSubscriptionsAsync(CancellationToken ct)
     {
 		var client = _httpClientFactory.CreateClient("MetrcWebHooks");
-
-		// Now, this is important. This must match the inbound controller route as this is the 
-		// fnct Metrc will call with updates.
-		var receiverUrl =
-			$"{_opts.WebhookReceiverBaseUrl.TrimEnd('/')}/api/metrc-webhooks/packages/inbound";
-
-
-		throw new NotImplementedException();
-    }
+		using var resp = await client.GetAsync("/webhooks/v2", ct);
+		return await MetrcEnvelopeFactory.FromResponseAsync(resp, ct);
+	}
 
     public Task<ApiEnvelope> SubscribeToPackageWebhooksAsync(CancellationToken ct)
     {
