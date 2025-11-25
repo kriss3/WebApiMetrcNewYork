095dcea8c018c7ef40f518c5038093d0a78d7088
Kris <kris_s2@outlook.com>
2025-11-24 22:37:54 -0800

    kata: attempt to configure a callback from Metrc back to my api.

---
+++ WebApiMetrcNewYork.App/Services/MetrcWebHooksService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Options;
+using WebApiMetrcNewYork.App.Models;
+
+namespace WebApiMetrcNewYork.App.Services;
+
+public sealed class MetrcWebHooksService(
+    IHttpClientFactory httpClientFactory,
+	IOptions<MetrcOptions> opts) : IMetrcWebHooksService
+{
+	private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+	private readonly MetrcOptions _opts = opts.Value;
+
+	public Task<ApiEnvelope> GetSubscriptionsAsync(CancellationToken ct)
+    {
+		var client = _httpClientFactory.CreateClient("MetrcWebHooks");
+
+		// Now, this is important. This must match the inbound controller route as this is the 
+		// fnct Metrc will call with updates.
+		var receiverUrl =
+			$"{_opts.WebhookReceiverBaseUrl.TrimEnd('/')}/api/metrc-webhooks/packages/inbound";
+
+
+		throw new NotImplementedException();
+    }
+
+    public Task<ApiEnvelope> SubscribeToPackageWebhooksAsync(CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
