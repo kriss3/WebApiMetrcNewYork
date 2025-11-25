dd4be810ee4afda2b3387ec100cce0f9620c27d6
Kris <kris_s2@outlook.com>
2025-11-24 22:27:39 -0800

    kata: adding service as a wrapper around webhooks operations.

---
+++ WebApiMetrcNewYork.App/Services/IMetrcWebhooksService.cs
@@ -0,0 +1,9 @@
+﻿using WebApiMetrcNewYork.App.Models;
+
+namespace WebApiMetrcNewYork.App.Services;
+
+public interface IMetrcWebhooksService
+{
+	Task<ApiEnvelope> SubscribeToPackageWebhooksAsync(CancellationToken ct);
+	Task<ApiEnvelope> GetSubscriptionsAsync(CancellationToken ct);
+}
